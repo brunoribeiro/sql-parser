c999bb9830431a1bbdc7c767bf2b33a262b31cf6
Mike McMahon <mmcm@comcast.net>
2011-03-21 13:55:41 -0400

    More of the server.

--- src/main/java/com/akiban/sql/pg/PostgresServer.java
+++ src/main/java/com/akiban/sql/pg/PostgresServer.java
@@ -28,7 +28,7 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
   public static final int DEFAULT_PORT = 15432; // Real one is 5432
 
   /*** Message Formats ***/
-  public static final int AUTHENTICATION_OK_TYPE = 'R'; // (B)
+  public static final int AUTHENTICATION_TYPE = 'R'; // (B)
   public static final int BACKEND_KEY_DATA_TYPE = 'K'; // (B)
   public static final int BIND_TYPE = 'B'; // (F)
   public static final int BIND_COMPLETE_TYPE = '2'; // (B)
@@ -61,13 +61,24 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
   public static final int QUERY_TYPE = 'Q'; // (F)
   public static final int READY_FOR_QUERY_TYPE = 'Z'; // (B)
   public static final int ROW_DESCRIPTION_TYPE = 'T'; // (B)
-  public static final int STARTUP_MESSAGE_TYPE = -1; // (F)
+  public static final int STARTUP_MESSAGE_TYPE = 0; // (F)
   public static final int SYNC_TYPE = 'S'; // (F)
   public static final int TERMINATE_TYPE = 'X'; // (F)
   
   public static final int VERSION_CANCEL = 80877102; // 12345678
   public static final int VERSION_SSL = 80877103; // 12345679
   
+  public static final int AUTHENTICATION_OK = 0;
+  public static final int AUTHENTICATION_KERBEROS_V5 = 2;
+  public static final int AUTHENTICATION_CLEAR_TEXT = 3;
+  public static final int AUTHENTICATION_MD5 = 5;
+  public static final int AUTHENTICATION_SCM = 6;
+  public static final int AUTHENTICATION_GSS = 7;
+  public static final int AUTHENTICATION_SSPI = 9;
+  public static final int AUTHENTICATION_GSS_CONTINUE = 8;
+
+  public static final int VARCHAR_TYPE_OID = 1043;
+
   public static void main(String[] args) throws Exception {
     int port = DEFAULT_PORT;
     int i = 0;
@@ -106,6 +117,26 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
     }
   }
 
+  static class DummyPreparedStatement {
+    String m_sql;
+    int[] m_paramTypes;
+    DummyPreparedStatement(String sql, int[] paramTypes) {
+      m_sql = sql;
+      m_paramTypes = paramTypes;
+    }
+  }
+  static class DummyStatementBinding {
+    DummyPreparedStatement m_statement;
+    Object[] m_parameters;
+    boolean[] m_resultsBinary;
+    DummyStatementBinding(DummyPreparedStatement statement, Object[] parameters, 
+                          boolean[] resultsBinary) {
+      m_statement = statement;
+      m_parameters = parameters;
+      m_resultsBinary = resultsBinary;
+    }
+  }
+
   private Socket m_socket;
   private InputStream m_inputStream;
   private OutputStream m_outputStream;
@@ -118,7 +149,11 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
   private int m_pid, m_secret;
   private int m_version;
   private Properties m_properties;
-  
+  private Map<String,DummyPreparedStatement> m_preparedStatements =
+    new HashMap<String,DummyPreparedStatement>();
+  private Map<String,DummyStatementBinding> m_boundPortals =
+    new HashMap<String,DummyStatementBinding>();
+
   public PostgresServer(Socket socket, int pid, int secret) {
     m_socket = socket;
     m_pid = pid;
@@ -166,6 +201,27 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
     while (m_running) {
       int type = readMessage();
       switch (type) {
+      case -1:                  // EOF
+        m_running = false;
+        break;
+      case PASSWORD_MESSAGE_TYPE:
+        processPasswordMessage();
+        break;
+      case PARSE_TYPE:
+        processParse();
+        break;
+      case BIND_TYPE:
+        processBind();
+        break;
+      case DESCRIBE_TYPE:
+        processDescribe();
+        break;
+      case EXECUTE_TYPE:
+        processExecute();
+        break;
+      case SYNC_TYPE:
+        processSync();
+        break;
       case TERMINATE_TYPE:
         processTerminate();
         break;
@@ -203,6 +259,11 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
       else
         m_encoding = enc;
     }
+    {
+      beginMessage(AUTHENTICATION_TYPE);
+      writeInt(AUTHENTICATION_CLEAR_TEXT);
+      sendMessage(true);
+    }
   }
 
   protected void processCancelMessage() throws Exception {
@@ -219,6 +280,163 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
     throw new Exception("NIY");
   }
 
+  protected void processPasswordMessage() throws Exception {
+    String user = m_properties.getProperty("user");
+    String pass = readString();
+    System.out.println(user + "/" + pass);
+    Properties status = new Properties();
+    // This is enough to make the JDBC driver happy.
+    status.put("client_encoding", m_properties.getProperty("client_encoding"));
+    status.put("server_encoding", m_encoding);
+    status.put("server_version", "8.4.7"); // Not sure what the min it'll accept is.
+    status.put("session_authorization", user);
+    
+    {
+      beginMessage(AUTHENTICATION_TYPE);
+      writeInt(AUTHENTICATION_OK);
+      sendMessage();
+    }
+    for (String prop : status.stringPropertyNames()) {
+      beginMessage(PARAMETER_STATUS_TYPE);
+      writeString(prop);
+      writeString(status.getProperty(prop));
+      sendMessage();
+    }
+    {
+      beginMessage(BACKEND_KEY_DATA_TYPE);
+      writeInt(m_pid);
+      writeInt(m_secret);
+      sendMessage();
+    }
+    {
+      beginMessage(READY_FOR_QUERY_TYPE);
+      writeByte('I');           // Idle ('T' -> xact open; 'E' -> xact abort)
+      sendMessage(true);
+    }
+  }
+
+  protected void processParse() throws Exception {
+    String stmtName = readString();
+    String query = readString();
+    short nparams = readShort();
+    int[] paramTypes = new int[nparams];
+    for (int i = 0; i < nparams; i++)
+      paramTypes[i] = readInt();
+    m_preparedStatements.put(stmtName, new DummyPreparedStatement(query, paramTypes));
+    beginMessage(PARSE_COMPLETE_TYPE);
+    sendMessage();
+  }
+
+  protected void processBind() throws Exception {
+    String portalName = readString();
+    String stmtName = readString();
+    short nformats = readShort();
+    boolean[] paramsBinary = new boolean[nformats];
+    for (int i = 0; i < nformats; i++)
+      paramsBinary[i] = (readShort() == 1);
+    short nparams = readShort();
+    Object[] params = new Object[nparams];
+    boolean binary = false;
+    for (int i = 0; i < nparams; i++) {
+      if (i < nformats)
+        binary = paramsBinary[i];
+      int len = readInt();
+      if (len < 0) continue;    // Null
+      byte[] param = new byte[len];
+      readFully(param, 0, len);
+      if (binary) {
+        params[i] = param;
+      }
+      else {
+        params[i] = new String(param, m_encoding);
+      }
+    }
+    short nresults = readShort();
+    boolean[] resultsBinary = new boolean[nresults];
+    for (int i = 0; i < nresults; i++) {
+      resultsBinary[i] = (readShort() == 1);
+    }
+    DummyPreparedStatement stmt = m_preparedStatements.get(stmtName);
+    m_boundPortals.put(portalName, 
+                       new DummyStatementBinding(stmt, params, resultsBinary));
+    beginMessage(BIND_COMPLETE_TYPE);
+    sendMessage();
+  }
+
+  protected void processDescribe() throws Exception {
+    byte source = readByte();
+    String name = readString();
+    DummyPreparedStatement stmt;
+    boolean[] resultsBinary = null;
+    switch (source) {
+    case (byte)'S':
+      stmt = m_preparedStatements.get(name);
+      break;
+    case (byte)'P':
+      stmt = m_boundPortals.get(name).m_statement;
+      break;
+    default:
+      throw new Exception("Unknown describe source: " + (char)source);
+    }
+    beginMessage(ROW_DESCRIPTION_TYPE);
+    int nfields = 2;
+    writeShort(nfields);
+    boolean binary = false;
+    for (int i = 0; i < nfields; i++) {
+      if ((resultsBinary != null) && (i < resultsBinary.length))
+        binary = resultsBinary[i];
+      writeInt(0);              // OID of table.
+      writeShort(0);            // colno in table.
+      writeInt(VARCHAR_TYPE_OID); // data type
+      writeShort(-1);           // typlen
+      writeInt(0);              // atttypmod
+      writeShort(binary ? 0 : 1);
+    }
+  }
+
+  protected void processExecute() throws Exception {
+    String portalName = readString();
+    int maxrows = readInt();
+    DummyStatementBinding binding = m_boundPortals.get(portalName);
+    int nrows = 3;
+    if (nrows == 0) {
+      beginMessage(NO_DATA_TYPE);
+      sendMessage();
+    }
+    else {
+      if ((maxrows > 0) && (maxrows < nrows))
+        nrows = maxrows;
+      short ncols = 2;
+      for (int j = 0; j < nrows; j++) {
+        beginMessage(DATA_ROW_TYPE);
+        writeShort(ncols);
+        boolean binary = false;
+        for (int i = 0; i < ncols; i++) {
+          if (i < binding.m_resultsBinary.length)
+            binary = binding.m_resultsBinary[i];
+          String value = (i < 1) ? Integer.toString(i) : null;
+          int len = (value == null) ? -1 : value.length();
+          byte[] bv = null;
+          if ((len > 0) && !binary)
+            bv = value.getBytes(m_encoding);
+          writeInt(len);
+          if (bv != null)
+            write(bv);
+        }
+        sendMessage();
+      }
+    }
+    beginMessage(COMMAND_COMPLETE_TYPE);
+    writeString("SELECT");
+    sendMessage();
+  }
+
+  protected void processSync() throws Exception {
+    beginMessage(READY_FOR_QUERY_TYPE);
+    writeByte('I');           // Idle ('T' -> xact open; 'E' -> xact abort)
+    sendMessage(true);
+  }
+
   protected void processTerminate() throws Exception {
     m_running = false;
   }
@@ -233,6 +451,8 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
       type = m_dataInput.read();
     else
       type = STARTUP_MESSAGE_TYPE;
+    if (type < 0) 
+      return type;              // EOF
     int len = m_dataInput.readInt();
     len -= 4;
     byte[] msg = new byte[len];
@@ -249,6 +469,10 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
   }
 
   protected void sendMessage() throws IOException {
+    sendMessage(false);
+  }
+
+  protected void sendMessage(boolean flush) throws IOException {
     m_messageOutput.flush();
     byte[] msg = m_byteOutput.toByteArray();
     int len = msg.length - 1;
@@ -257,6 +481,7 @@ public class PostgresServer implements Runnable, DataInput, DataOutput
     msg[3] = (byte)(len >> 8);
     msg[4] = (byte)len;
     m_outputStream.write(msg);
+    if (flush) m_outputStream.flush();
   }
 
   /** Read null-terminated string. */
