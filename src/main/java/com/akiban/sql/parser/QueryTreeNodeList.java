bb0eee9873f7eec4bde0943d511e0774680f2b71
Mike McMahon <mmcm@comcast.net>
2012-03-27 11:19:42 -0400

    Adjust unparser.

--- src/main/java/com/akiban/sql/parser/QueryTreeNodeList.java
+++ src/main/java/com/akiban/sql/parser/QueryTreeNodeList.java
@@ -60,6 +60,10 @@ public abstract class QueryTreeNodeList<N extends QueryTreeNode>
         return list.size();
     }
 
+    public final boolean isEmpty() {
+        return list.isEmpty();
+    }
+
     public N get(int index) {
         return list.get(index);
     }
