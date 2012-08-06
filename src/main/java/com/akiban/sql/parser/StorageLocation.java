a9d0fa50190fbe5be1d6e055ba7357c93e4f9729
Vy Nguyen <oontvoo@hotmail.com>
2012-08-06 16:12:01 -0400

    remove IDE's boilerplates

--- src/main/java/com/akiban/sql/parser/StorageLocation.java
+++ src/main/java/com/akiban/sql/parser/StorageLocation.java
@@ -23,10 +23,7 @@
  * USE OF THE SOFTWARE, THE TERMS AND CONDITIONS OF SUCH OTHER AGREEMENT SHALL
  * PREVAIL OVER ANY CONFLICTING TERMS OR CONDITIONS IN THIS AGREEMENT.
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.akiban.sql.parser;
 
 public enum StorageLocation { IN_MEMORY, BTREE };
