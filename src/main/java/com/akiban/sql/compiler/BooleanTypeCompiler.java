a99cba73fc24595ad2d660b903569d9bcd82a1dd
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2012-11-07 12:49:31 -0500

    Add EPL license header to Java files.

--- src/main/java/com/akiban/sql/compiler/BooleanTypeCompiler.java
+++ src/main/java/com/akiban/sql/compiler/BooleanTypeCompiler.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright © 2012 Akiban Technologies, Inc.  All rights
+ * reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This program may also be available under different license terms.
+ * For more information, see www.akiban.com or contact
+ * licensing@akiban.com.
+ *
+ * Contributors:
+ * Akiban Technologies, Inc.
+ */
  * END USER LICENSE AGREEMENT (“EULA”)
  *
  * READ THIS AGREEMENT CAREFULLY (date: 9/13/2011):
--- src/main/java/com/akiban/sql/views/ViewDefinition.java
+++ src/main/java/com/akiban/sql/views/ViewDefinition.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright © 2012 Akiban Technologies, Inc.  All rights
+ * reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This program may also be available under different license terms.
+ * For more information, see www.akiban.com or contact
+ * licensing@akiban.com.
+ *
+ * Contributors:
+ * Akiban Technologies, Inc.
+ */
 
 package com.akiban.sql.views;
 
